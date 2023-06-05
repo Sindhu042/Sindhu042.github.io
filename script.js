9d7835747d9a5acbc91877ea13b12a649b28ff58
Sindhu042 <92875064+Sindhu042@users.noreply.github.com>
2023-06-05 21:40:45 +0530

    performed first DOM manipulation

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Welcome back!")
+var data1 = {image : "photo2.jpeg",  name : "JANJARAPU SINDHUJA", deisgnation : "Student"};
+var data2 = {image : "photo3.jpeg",  name : "SINDHU", deisgnation : "Software Intern"};
+var isData1 = true;
+var displayData;
+var changePic = function(){
+    if(isData1){
+        displayData = data2;
+        isData1 = false;
+    }
+    else{
+        displayData = data1;
+        isData1 = true;
+    }
+    document.getElementById("myImage").src = displayData.image;
+}
